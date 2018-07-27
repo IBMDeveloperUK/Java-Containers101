182b652aa31209b553c8b5f72f189d6598918057
Mofe Salami <tsalami@uk.ibm.com>
2018-07-27 18:20:08 +0100

    Removed unused route

--- src/main/java/com/ibm/code/java/ping/PingController.java
+++ src/main/java/com/ibm/code/java/ping/PingController.java
@@ -1,7 +1,6 @@
 package com.ibm.code.java.ping;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,15 +9,5 @@ class PingController{
     @GetMapping("/ping")
     public String getPing(){
         return "pong";
-    }
-
-    @GetMapping("/ping/{delay}")
-    public String getPingWithDelayInMilliSeconds(@PathVariable int delay){
-        try {
-                        Thread.sleep(delay);
-                } catch (InterruptedException e) {
-                        e.printStackTrace();
-                }
-        return "pong after " + delay + " milliseconds delay";
     }
 }
